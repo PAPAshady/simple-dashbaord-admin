5c9dcb6bd8e9c963c0febe3ecb2f8c8c758d29e5
Nima Zamani <zamani.nima18@gmail.com>
2024-12-11 15:27:57 +0330

    feat(users page): added remove user fuctionality and snack bar

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import profilePic from "../../assets/images/profile-pic.webp";
-import noUserImg from "../../assets/images/no-user-img.png";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Snackbar } from "@mui/material";
+import usersArray from "./usersData";
 import "./Users.css";
 
 function Users() {
+  const [users, setUsers] = useState(usersArray);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const removeUserHandler = (userID) => {
+    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userID));
+    setSnackbarMessage("User removed successfully");
+    setOpenSnackbar(true);
+  };
+
+  useEffect(() => {
+    if (openSnackbar) {
+      setTimeout(() => setOpenSnackbar(false), 3000);
+    }
+  }, [openSnackbar]);
+
   const columns = [
     { id: 1, field: "id", headerName: "ID" },
     {
@@ -32,10 +48,13 @@ function Users() {
       field: "",
       headerName: "Action",
       width: 150,
-      renderCell: () => (
+      renderCell: ({ row }) => (
         <div className="users-table__actions">
           <button className="users-table__action-edit-btn">Edit</button>
-          <button className="users-table__action-remove-btn">
+          <button
+            className="users-table__action-remove-btn"
+            onClick={() => removeUserHandler(row.id)}
+          >
             <DeleteIcon />
           </button>
         </div>
@@ -43,70 +62,22 @@ function Users() {
     },
   ];
 
-  const rows = [
-    {
-      id: 1,
-      userName: "Nima Zamani",
-      avatar: profilePic,
-      status: "Active",
-      transaction: "$55.99",
-      email: "Zamani.nima18@gmail.com",
-    },
-    {
-      id: 2,
-      userName: "Akbar Zamani",
-      avatar: noUserImg,
-      status: "Not-active",
-      transaction: "$50",
-      email: "Zamani.nima18@gmail.com",
-    },
-    {
-      id: 3,
-      userName: "Keyvan Zamani",
-      avatar: noUserImg,
-      status: "not-Active",
-      transaction: "$21",
-      email: "Zamani.nima18@gmail.com",
-    },
-    {
-      id: 4,
-      userName: "Soodabeh Badiyeneshin",
-      avatar: profilePic,
-      status: "Active",
-      transaction: "$981",
-      email: "Zamani.nima18@gmail.com",
-    },
-    {
-      id: 5,
-      userName: "renadom person",
-      avatar: profilePic,
-      status: "Active",
-      transaction: "$19",
-      email: "Zamani.nima18@gmail.com",
-    },
-    {
-      id: 6,
-      userName: "a chill guy",
-      avatar: profilePic,
-      status: "Active",
-      transaction: "$19000",
-      email: "Zamani.nima18@gmail.com",
-    },
-  ];
-
   return (
-    <div className="users-table">
-      <DataGrid
-        columns={columns}
-        rows={rows}
-        disableRowSelectionOnClick
-        initialState={{
-          pagination: {
-            paginationModel: { pageSize: 3, page: 0 },
-          },
-        }}
-      />
-    </div>
+    <>
+      <div className="users-table">
+        <DataGrid
+          columns={columns}
+          rows={users}
+          disableRowSelectionOnClick
+          // initialState={{
+          //   pagination: {
+          //     paginationModel: { pageSize: 3, page: 0 },
+          //   },
+          // }}
+        />
+      </div>
+      <Snackbar open={openSnackbar} message={snackbarMessage} />
+    </>
   );
 }
 
---
+++ src/pages/Users/usersData.js
@@ -0,0 +1,55 @@
+import profilePic from "../../assets/images/profile-pic.webp";
+import noUserImg from "../../assets/images/no-user-img.png";
+
+const users = [
+  {
+    id: 1,
+    userName: "Nima Zamani",
+    avatar: profilePic,
+    status: "Active",
+    transaction: "$55.99",
+    email: "Zamani.nima18@gmail.com",
+  },
+  {
+    id: 2,
+    userName: "Akbar Zamani",
+    avatar: noUserImg,
+    status: "Not-active",
+    transaction: "$50",
+    email: "Zamani.nima18@gmail.com",
+  },
+  {
+    id: 3,
+    userName: "Keyvan Zamani",
+    avatar: noUserImg,
+    status: "not-Active",
+    transaction: "$21",
+    email: "Zamani.nima18@gmail.com",
+  },
+  {
+    id: 4,
+    userName: "Soodabeh Badiyeneshin",
+    avatar: profilePic,
+    status: "Active",
+    transaction: "$981",
+    email: "Zamani.nima18@gmail.com",
+  },
+  {
+    id: 5,
+    userName: "renadom person",
+    avatar: profilePic,
+    status: "Active",
+    transaction: "$19",
+    email: "Zamani.nima18@gmail.com",
+  },
+  {
+    id: 6,
+    userName: "a chill guy",
+    avatar: profilePic,
+    status: "Active",
+    transaction: "$19000",
+    email: "Zamani.nima18@gmail.com",
+  },
+];
+
+export default users;
